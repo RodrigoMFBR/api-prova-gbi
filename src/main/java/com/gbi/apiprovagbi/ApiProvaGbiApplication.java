df7855a739c12a0c4e5a7852b773f57d02e1e067
Rodrigo <a11077@centralnacionalunimed.com.br>
2024-02-01 10:02:20 -0300

    Commit inicial

---
+++ src/main/java/com/gbi/apiprovagbi/ApiProvaGbiApplication.java
@@ -0,0 +1,13 @@
+package com.gbi.apiprovagbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProvaGbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProvaGbiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbi/apiprovagbi/ApiProvaGbiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbi.apiprovagbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProvaGbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
